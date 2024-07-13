cace8333229d67fa5b048da5778feac22cdff66a
David Koontz <david@koontz.name>
2024-07-13 14:23:25 -0700

    Initial Commit

---
+++ PWpro/PWproApp.swift
@@ -0,0 +1,17 @@
+//
+//  PWproApp.swift
+//  PWpro
+//
+//  Created by David on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PWproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
